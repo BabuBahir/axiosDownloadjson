3b3e20d473b273a986f27146f34f63fbdaa7502b
rohit <indians4german@gmail.com>
2017-10-04 22:20:57 +0530

    first

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ var app = express();
 var router = express.Router();
 var axios = require("axios");
 var bodyParser = require('body-parser');
+var fs = require('fs');
 var path = __dirname + '/views/';
-
+var filepath = __dirname + '/downloadtoData.js';
 
 router.use(function (req,res,next) {
   console.log("/" + req.method);
@@ -18,10 +19,15 @@ router.get("/",function(req,res){
 router.post("/getdata",function(req,res){
     console.log(req.body);
     axios.get('https://jsonplaceholder.typicode.com/posts')
-    .then(function(response){
-      res.send(response.data);  
-        
-    });  
+    .then(function(response){   
+            
+                fs.writeFile(filepath, response.data[1].body, 'utf8', function (err) {
+                    if (err) {
+                        return console.log(err);
+                    }
+                    res.download(filepath);
+                });                 
+    });      
 });
 
 app.use(bodyParser.urlencoded({ extended: false }));
