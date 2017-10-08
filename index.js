cdb152de78a71d2f07bd510a53bd5abea0bfe875
rohit <indians4german@gmail.com>
2017-10-08 23:48:31 +0530

    x ray woriking

--- index.js
+++ index.js
@@ -14,6 +14,21 @@ var cheerio = require('cheerio');
 var moment = require('moment');
 var xray = require('x-ray');
 var x = xray();
+const makeDriver = require('request-x-ray');
+
+
+const options = {
+	method: "GET", 						//Set HTTP method
+	jar: true, 							//Enable cookies
+	headers: {							//Set headers
+		"User-Agent": "Firefox/48.0"
+	}
+};
+
+const driver = makeDriver(options);		//Create driver
+
+x.driver(driver);
+
 
 router.use(function (req, res, next) {
   next();
@@ -31,34 +46,90 @@ router.get("/getdata", function (req, res) {
       toDate = moment(toDate).format('DD/MM/YYYY');
       var ctr = 0;  
       var nodeArr = [];
-      var Maxitteration = 5;
-     
+      var itteration = 0;
+      
 
       var Vurl = 'https://www.localgov.ie/en/bcms/search?search_api_views_fulltext=' +
-        '&validation_date_from[date]=' + fromDate + '&validation_date_to[date]=' + toDate +
-        '&page=';
-      
+        '&validation_date_from[date]=' + fromDate + '&validation_date_to[date]=' + toDate ;         
        
-      for(var itteration =0 ; itteration < Maxitteration ; itteration++){
-         tempurl = Vurl + itteration;
-            
-         console.log(tempurl);
-
-          x(tempurl, 'body@html')(function(err, body) {
-            $ = cheerio.load(body);                     
- 
-            $("a.btn-small").each(function (i, item) {
-                var sectionhref = $(this).attr("href");
-                ctr++;
-                nodeArr.push(  { ctr , sectionhref }) ; // Google                 
+         tempurl = Vurl  ;                      
+   
+         scrapeUrl(tempurl, function(err, obj) {
+          if(err) {
+            console.log(err);
+          } else {
+            obj.forEach((linker) => {
+              var link = linker.link;
+              
+              genericScrape(link, (err, obj) => {
+                if(obj.title) {
+                  console.log(util.format("%s :::: %s", obj.title.trim(), obj.links.trim()))
+                }
+              });
             });
-            console.log(nodeArr);
-              if(err){ console.log(err); }
-          });
-
-      }
+          }
+        }); 
+       
     
   });
+
+
+ 
+var scrapeUrl = function(url, callback) {
+ 
+  x(url, 'li.views-row', [{
+  title: '.search-results-title',
+  
+}]) .paginate('.pager-item > a@href').limit(5)
+  (function(err, obj) {
+    if(err || !obj) {
+      console.log('An exception occured.')
+      //callback(err);
+      return;
+    }
+    console.log(obj)
+    obj.forEach((item) => {
+      var start = item.link.indexOf('q=')
+      var end = item.link.indexOf('&sa')
+      item.link = item.link.substring(start + 2, end)
+    })
+
+    callback(null, obj)
+  })
+}
+
+
+  var genericScrape = function(url, callback) {
+  
+    x(url, 'html', {
+      title: 'title',
+      links: 'a@href'
+    })((err, obj) => {
+      if(err || !obj) {
+        callback(err)
+        return;
+      }
+      callback(null, obj);
+    });
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     router.get("/node/:ID", function (req, res) {
